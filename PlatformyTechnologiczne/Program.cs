5b1863e4a88bd188ff519c3de4ab3b07680e0bc4
Karol Bereśniewicz <karol@beresniewicz.dev>
2019-04-13 11:05:05 +0200

    Serialization and deserialization works with File as backing storage.

--- PlatformyTechnologiczne/Program.cs
+++ PlatformyTechnologiczne/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace PlatformyTechnologiczne
@@ -69,8 +70,8 @@ namespace PlatformyTechnologiczne
 
 
             var oldestChildren = dirInfo.OldestChildrenDate(fsi => fsi.LastWriteTime);
-            Console.WriteLine(oldestChildren != DateTime.MaxValue 
-                                                ? oldestChildren.ToString() 
+            Console.WriteLine(oldestChildren != DateTime.MaxValue
+                                                ? oldestChildren.ToString()
                                                 : "Directory with given path is empty or the file system is corrupted");
 
 
@@ -82,8 +83,10 @@ namespace PlatformyTechnologiczne
 
             dirInfo = new DirectoryInfo(directory);
             var sortedCollection = new SortedDictionary<string, long>(Comparer<string>.Create((a, b) => a.Length == b.Length ?
-                                                                                                      a.CompareTo(b) : 
+                                                                                                      a.CompareTo(b) :
                                                                                                       a.Length > b.Length ? 1 : -1));
+
+            Console.Write("\n\nZadanie 5.a \n\n");
 
             dirInfo.EnumerateFileSystemInfos()
                 .ToList()
@@ -93,12 +96,53 @@ namespace PlatformyTechnologiczne
                     // Adding name and amount of children if it's a directory and file size otherwise
                     sortedCollection.Add(x.FullName, isDirectory ?
                                                              Directory.EnumerateFileSystemEntries(x.FullName).LongCount() :
-                                                             new FileInfo(x.FullName).Length);  
+                                                             new FileInfo(x.FullName).Length);
                 }
                 );
 
-            sortedCollection.ToList().ForEach(kv => Console.WriteLine(kv.Key));
 
+            Console.WriteLine("Before serialization: \n");
+            sortedCollection.ToList().ForEach(kv => Console.WriteLine($"{kv.Key} : {kv.Value}"));
+
+            Console.WriteLine("\n\n Zadanie 5.b\n\n");
+
+            var binaryFormatter = new BinaryFormatter();
+
+            var kilo = (int)Math.Pow(2, 10);
+
+            var serializedFilePath = "serializedCollection";
+
+            using (var serializedDest = File.Create("serializedCollection"))
+            {
+                try
+                {
+                    binaryFormatter.Serialize(serializedDest, sortedCollection.ToList());
+                    serializedDest.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Failed to serialize cuz: ${e.Message}");
+                }
+            }
+
+            using (var serializedSource = File.OpenRead(serializedFilePath))
+            {
+                try
+                {
+
+                    var deserialized = (List<KeyValuePair<string, long>>)binaryFormatter.Deserialize(serializedSource);
+                    Console.WriteLine("After serialization");
+                    sortedCollection.Clear();
+                    Console.WriteLine($"Initial collection size: {sortedCollection.Count()}");
+                    deserialized.ForEach(x => sortedCollection.Add(x.Key, x.Value));
+                    sortedCollection.ToList().ForEach(x => Console.WriteLine($"{x.Key} -> {x.Value}"));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 
