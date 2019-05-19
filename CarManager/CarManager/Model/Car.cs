37d26177b9e058bb33d13d8d1010bc2d4e0ebc05
Karol Bereśniewicz <karol@beresniewicz.dev>
2019-05-19 13:05:05 +0200

    Added model.

---
+++ CarManager/CarManager/Model/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManager.Model
+{
+    public class Car
+    {
+        public string Model;
+        public Engine Motor;
+        public int Year;
+
+        public Car(string model, Engine engine, int year)
+        {
+            Model = model;
+            Motor = engine;
+            Year = year;
+        }
+    }
+}
---
+++ CarManager/CarManager/Model/Engine.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManager.Model
+{
+    public class Engine
+    {
+        public double Displacement;
+        public double HorsePower;
+        public string Model;
+
+        public Engine(double displacement, double horsePower, string model)
+        {
+            Displacement = displacement;
+            HorsePower = horsePower;
+            Model = model;
+        }
+    }
+}
